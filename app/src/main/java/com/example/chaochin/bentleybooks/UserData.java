e5762693b463fdd5831eef0532be24802dc4b90c
liushun9497@gmail.com <ls()yykl117>
2018-04-07 15:48:13 -0400

    user data

---
+++ app/src/main/java/com/example/chaochin/bentleybooks/UserData.java
@@ -0,0 +1,26 @@
+package com.example.chaochin.bentleybooks;
+
+import android.app.Application;
+
+
+public class UserData extends Application{
+    private String password;
+    private String email;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
