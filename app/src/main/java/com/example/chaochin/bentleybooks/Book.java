b9563f26f5dcda2c7990ff2eeb3ac8710ba77bc0
Chin <CHAO_CHIN@bentley.edu>
2018-04-11 16:12:59 -0400

    add register xml

---
+++ app/src/main/java/com/example/chaochin/bentleybooks/Book.java
@@ -0,0 +1,26 @@
+package com.example.chaochin.bentleybooks;
+
+/**
+ * Created by chaochin on 2018/4/11.
+ */
+
+public class Book {
+    private String ISBN;
+    private String condition;
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+}
