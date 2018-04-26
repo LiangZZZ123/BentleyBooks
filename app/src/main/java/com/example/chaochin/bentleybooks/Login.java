a3879187f224a433b1f7e193d41ea1bb10de85e4
Chin <CHAO_CHIN@bentley.edu>
2018-04-25 21:42:23 -0400

    Login complete

--- app/src/main/java/com/example/chaochin/bentleybooks/Login.java
+++ app/src/main/java/com/example/chaochin/bentleybooks/Login.java
@@ -1,49 +1,19 @@
 package com.example.chaochin.bentleybooks;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.app.Activity;
+
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -58,6 +28,7 @@ public class Login extends AppCompatActivity  {
     public String email;
     private String password;
     private boolean loginInfo = false;
+    private boolean searchComplete = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +38,9 @@ public class Login extends AppCompatActivity  {
         e_password = (EditText)findViewById(R.id.password);
         signIn = (Button)findViewById(R.id.sign_in_button);
         signUp = (Button)findViewById(R.id.sign_up_button);
+        e_email.setHint("Please enter your Bentley email");
+        e_password.setHint("Password");
+
 
         signIn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -78,17 +52,32 @@ public class Login extends AppCompatActivity  {
                 } else {
                     Thread t1 = new Thread(background);
                     t1.start();
-
-                    if (loginInfo) {
-                        Intent intent1 = new Intent(Login.this, Search_ISBN.class);
-                        startActivity(intent1);
-                    }
-                    else{
-                        Toast.makeText(Login.this, "No match", Toast.LENGTH_LONG).show();
+                    try {
+                        t1.join();
+                    } catch (InterruptedException e) {
+                        Log.e("JDBC", "Interrupted Exception");
                     }
                 }
+                loginCheck();
             }
         });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent i2 = new Intent(Login.this, SignUp.class);
+                startActivity(i2);
+            }
+        });
+    }
+
+    public void loginCheck() {
+            if (loginInfo) {
+                Intent intent1 = new Intent(Login.this, Search_ISBN.class);
+                startActivity(intent1);
+                finish();
+            } else {
+                Toast.makeText(Login.this, "No account records", Toast.LENGTH_LONG).show();
+            }
     }
 
     private Runnable background = new Runnable() {
@@ -125,16 +114,15 @@ public class Login extends AppCompatActivity  {
                 ResultSet result = stmt.executeQuery(
                         "SELECT email, pw FROM user;");
 
-                //for each record in City table add City to ArrayList and add city data to log
+
                 while (result.next()) {
                     String emailFromDB = result.getString("email");
                     String pwFromDB = result.getString("pw");
-                    if(emailFromDB.equals(email)&&pwFromDB.equals(password)){
+                    if(emailFromDB.equals(email) && pwFromDB.equals(password)){
                         loginInfo = true;
+                        break;
                     }
                 }
-
-
             } catch (SQLException e) {
                 Log.e("JDBC","problems with SQL sent to "+URL+
                         ": "+e.getMessage());
@@ -152,6 +140,3 @@ public class Login extends AppCompatActivity  {
         }
     };
 }
-
-
-
